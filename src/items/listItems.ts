72046a6a947c15a36606c4cf541bf7b8b608279e
Dexx <probatos@gmail.com>
2019-02-27 08:52:25 +0100

    Fix inverted null check

--- src/items/listItems.ts
+++ src/items/listItems.ts
@@ -2,7 +2,7 @@ import { getUserData } from "../userData/userData";
 
 const itemType = process.argv[2];
 if (!itemType) {
-    getUserData(userData => console.log(userData.items[itemType] || "Specified item type does not exist"));
-} else {
     console.log("Specify an item type");
+} else {
+    getUserData(userData => console.log(userData.items[itemType] || `No results for item type ${itemType}`));
 }
